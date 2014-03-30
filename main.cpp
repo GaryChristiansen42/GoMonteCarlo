16207137ad156b51b6b8be21fd7a5ba5694be0b0
Gary Christiansen <digimon2.0@gmail.com>
2014-03-29 19:44:24 -0500

    Should have been in last commit.

--- main.cpp
+++ main.cpp
@@ -1,116 +1,140 @@
-#include "UCT.h"
+#include <assert.h>
 #include <time.h>
-#include <fstream>
 
-Node *root;
+#include <cstdlib>
+#include <cstdio>
+#include <string>
 
-void playerMove(Node** currentNode, Board* b)
-{
-	bool validInput = false;
-	Point choice(-1, -1);
-	while(!validInput)
-	{
-	
-		cout << "Row: ";
-		string input;
-		cin >> input;
-		choice.row = atoi(input.c_str());
-		
-		cout << "Column: ";
-		cin >> input;
-		choice.column = atoi(input.c_str());
-		
-		cout << "\n";
-		
-		if(b->isValidMove(choice))
-			validInput = true;
-		else
-			cout << "That position is not valid\n"; 
-	}	
-	
-	cout << "Row: " << choice.row << "\nColumn: " << choice.column << endl;
-	
-	b->makeMove(choice);
-	Board* bCopy = b->clone();
-	Node* newCurrentNode = new Node(choice, bCopy, (*currentNode));
-	(*currentNode)->addChild(newCurrentNode);
-	(*currentNode) = newCurrentNode;
+#include "UCT.h"
+
+UCTNode *root;
+
+void playerMove(UCTNode** currentNode, Board* b) {
+  bool validInput = false;
+  Point choice(-1, -1);
+  while (!validInput) {
+    printf("Row: ");
+    scanf("%d", &choice.row);
+
+    printf("Column: ");
+    scanf("%d", &choice.column);
+
+    printf("\n");
+
+    if (b->isValidMove(choice))
+      validInput = true;
+    else
+      printf("That position is not valid\n");
+  }
+
+  printf("Row: %d\nColumn: %d\n", choice.row, choice.column);
+
+  b->makeMove(choice);
+  UCTNode* newCurrentNode = new UCTNode(choice, b, NULL);
+  delete (*currentNode);
+  (*currentNode) = newCurrentNode;
 }
 
-void printTabs(ofstream* file, int numTabs)
-{
-	for(int i = 0; i < numTabs; i++)
-		(*file) << "\t";
+void printTabs(int numTabs) {
+  for (int i = 0; i < numTabs; i++)
+    printf("\t");
 }
 
-void showTree(ofstream* file, Node* root, int numTabs)
-{
-	if(root == NULL)
-		return;
-	printTabs(file, numTabs);
-	(*file) << "Node\n";
-	printTabs(file, numTabs);
-	(*file) << "Move: " << root->move.row << " " << root->move.column << endl;
-	printTabs(file, numTabs);
-	//(*file) << "Wins: " << root->wins << " Losses: " << root->losses << " Visits: " << root->visits << endl;
-	printTabs(file, numTabs);
-	(*file) << "Children: \n";
-	showTree(file, root->child, numTabs+1);
-	showTree(file, root->sibling, numTabs);
+void showTree(UCTNode* node, int numTabs, int maxDepth = 1) {
+  if (node == NULL || numTabs >= maxDepth || node->visits < 2)
+    return;
+  printTabs(numTabs);
+  printf("Node\n");
+  printTabs(numTabs);
+  printf("\tMove: %d %d\n", node->move.row, node->move.column);
+  printTabs(numTabs);
+  printf("\tRewards/Visits: %f %d\n", node->totalRewards, node->visits);
+  printTabs(numTabs);
+  // (*file) << "Wins: " << root->wins
+  // << " Losses: " << root->losses << " Visits: " << root->visits << endl;
+  printTabs(numTabs);
+  printf("Children: \n");
+  showTree(node->child, numTabs+1, maxDepth);
+  showTree(node->sibling, numTabs, maxDepth);
 }
 
-void computerMove(Node** currentNode, Board* b)
-{
-	Node* newCurrentNode = UCTSearch((*currentNode), b);
-	(*currentNode) = newCurrentNode;
-	b->makeMove((*currentNode)->move);
-	cout << "Row: " << (*currentNode)->move.row << "\nColumn: " << (*currentNode)->move.column << endl;
+void computerMove(UCTNode** currentNode, Board* b, int numSimulations) {
+  UCTNode* newCurrentNode = UCTSearch((*currentNode), b, numSimulations);
 
-	/*ofstream myfile;
-	myfile.open ("tree.txt");
-	showTree(&myfile, root, 0);
-	myfile.close();*/
+  (*currentNode)->removeChild(newCurrentNode);
+  delete (*currentNode);
+
+  (*currentNode) = newCurrentNode;
+  b->makeMove((*currentNode)->move);
+  // check if b == currentNode->state
+  for (int row = 0; row < b->boardSize; row++)
+    for (int column = 0; column < b->boardSize; column++)
+      if (b->positions[row][column]
+        != (*currentNode)->state->positions[row][column]) {
+        printf("Problem at %d %d\n", row, column);
+      }
+  if (!(*b == *(*currentNode)->state))
+    assert(false);
+
+  printf("Row: %d\nColumn: %d\n",
+    (*currentNode)->move.row, (*currentNode)->move.column);
+
+  /*ofstream myfile;
+  myfile.open ("tree.txt");
+  showTree(&myfile, root, 0);
+  myfile.close();*/
 }
 
-int main(int in, char* args[])
-{
-	cout << "TicTacToe MonteCarloAI\n";
+void randomMove(UCTNode** currentNode, Board* b) {
+  static unsigned int seed = time(NULL);
+  unsigned int choice = rand_r(&seed) % b->possibleMoves.size();
+  Point chosenMove = b->possibleMoves[choice];
 
-	srand ((unsigned int)time(NULL));
-	
-	Board* b = new Board();
-	b->initialize(Black);
-	
-	Node *currentNode;
-	Point m(-1, -1);
-	root = new Node(m, b->clone(), NULL);
-	currentNode = root;
-	
-	b->show();
+  printf("Row: %d\nColumn: %d\n", chosenMove.row, chosenMove.column);
 
-	GameResult result = None;
-	while(!b->isGameOver(&result))
-	{
-		if(b->turn == b->computer)
-			computerMove(&currentNode, b);
-		else
-			computerMove(&currentNode, b);
-			//playerMove(&currentNode, b);
-		b->show();
-	}
-	
-	if(result == Draw)
-		cout << "Draw\n";
-	else if((int)result == (int)Black)
-		cout << "Black Won\n";
-	else 
-		cout << "White Won\n";
-	
-	float whiteScore = 0, blackScore = 0;
-	b->getSimpleScore(&whiteScore, &blackScore);
-	cout << "\nWhite: " << whiteScore << "\nBlack: " << blackScore << endl;
-	
-	delete root;
-	delete b;
-	return 0;
+  b->makeMove(chosenMove);
+  UCTNode* newCurrentNode = new UCTNode(chosenMove, b, NULL);
+  delete (*currentNode);
+  (*currentNode) = newCurrentNode;
+}
+
+int main(void) {
+  printf("Go MonteCarloAI\n");
+
+  int boardSize = 5;
+  int numSimulations = 10000;
+
+  Board* b = new Board(boardSize);
+
+  UCTNode *currentNode;
+  root = new UCTNode(Point(-1, -1), b, NULL);
+  currentNode = root;
+
+  b->show();
+
+  GameResult result = None;
+  while (!b->isGameOver(&result)) {
+    if (b->turn == Black)
+      computerMove(&currentNode, b, numSimulations);
+    else
+      // computerMove(&currentNode, b);
+      playerMove(&currentNode, b);
+      // randomMove(&currentNode, b);
+    b->show();
+  }
+
+  if (result == Draw)
+    printf("Draw\n");
+  else if (static_cast<Player>(result) == Black)
+    printf("Black Won\n");
+  else
+    printf("White Won\n");
+
+  float whiteScore = 0, blackScore = 0;
+  b->getSimpleScore(&whiteScore, &blackScore);
+  printf("\nWhite: %f\nBlack: %f\n", whiteScore, blackScore);
+
+  delete currentNode;
+  delete b;
+  return 0;
 }
