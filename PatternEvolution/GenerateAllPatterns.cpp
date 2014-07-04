efe4f646111aa48e43ce990123b040f5b6cc1e23
Gary Christiansen <digimon2.0@gmail.com>
2014-07-04 00:12:14 -0500

    Made GenerateAllPatterns which generates all the 3x3 patterns possible.

---
+++ PatternEvolution/GenerateAllPatterns.cpp
@@ -0,0 +1,125 @@
+#include <assert.h>
+
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <string>
+
+#include "../Patterns.h"
+
+
+struct pattern_compare {
+  bool operator() (const Pattern& lhs, const Pattern& rhs) const{
+    return lhs.hash < rhs.hash;
+  }
+};
+
+int main() {
+
+  Patterns patterns;
+
+  std::vector<std::string> strings;
+  std::string temp = "adddddddd";
+  for (temp[0] = 'a' ; temp[0] <= 'd' ; temp[0]++)
+    for (temp[1] = 'a' ; temp[1] <= 'd' ; temp[1]++)
+      for (temp[2] = 'a' ; temp[2] <= 'd' ; temp[2]++)
+        for (temp[3] = 'a' ; temp[3] <= 'd' ; temp[3]++)
+          for (temp[4] = 'a' ; temp[4] <= 'd' ; temp[4]++)
+            for (temp[5] = 'a' ; temp[5] <= 'd' ; temp[5]++)
+              for (temp[6] = 'a' ; temp[6] <= 'd' ; temp[6]++)
+                for (temp[7] = 'a' ; temp[7] <= 'd' ; temp[7]++)
+                  for (temp[8] = 'a' ; temp[8] <= 'd' ; temp[8]++)
+                    strings.push_back(temp);
+
+  for (auto& hash : strings) {
+    for (auto& c : hash) {
+      if (c == 'a')
+        c = 'B';
+      else if (c == 'b')
+        c = 'W';
+      else if (c == 'c')
+        c = 'O';
+      else if (c == 'd')
+        c = '_';
+      else
+        assert(false);
+    }
+
+    Pattern p;
+    p.hash = hash;
+    p.determineRandomGoodMoves();
+
+    patterns.hashTable[hash] = p;
+
+  }
+
+  /*for (auto x : patterns.hashTable) {
+    std::cout << x.second << std::endl;
+  }*/
+  std::cout << patterns.hashTable.size() << std::endl;
+  std::cout << (patterns.hashTable["BBBBBBBBB"]) << std::endl;
+
+  std::vector<Pattern> legalGeneratedPatterns;
+  for (auto it = patterns.hashTable.begin(); it != patterns.hashTable.end(); ++it) {
+    if ((*it).second.isLegalPattern()) {
+      legalGeneratedPatterns.push_back((*it).second);
+    }
+  }
+  std::cout << legalGeneratedPatterns.size() << std::endl;
+
+  unsigned long int i = 0;
+  for (auto x : legalGeneratedPatterns) {
+    for (auto c : x.hash) {
+      if (c == '_') {
+        ++i;
+      }
+    }
+  }
+  std::cout << i << std::endl;
+
+  std::set<Pattern, pattern_compare> notDoubles1;
+  for (auto x : legalGeneratedPatterns) {
+    Pattern p = x;
+    if (notDoubles1.find(p) != notDoubles1.end())
+      continue;
+    p.rotate90();
+    if (notDoubles1.find(p) != notDoubles1.end())
+      continue;
+    p.rotate90();
+    if (notDoubles1.find(p) != notDoubles1.end())
+      continue;
+    p.rotate90();
+    if (notDoubles1.find(p) != notDoubles1.end())
+      continue;
+    p.invertColor();
+    if (notDoubles1.find(p) != notDoubles1.end())
+      continue;
+    p.rotate90();
+    if (notDoubles1.find(p) != notDoubles1.end())
+      continue;
+    p.rotate90();
+    if (notDoubles1.find(p) != notDoubles1.end())
+      continue;
+    p.rotate90();
+    if (notDoubles1.find(p) != notDoubles1.end())
+      continue;
+    notDoubles1.insert(p);
+  }
+  std::cout << notDoubles1.size() << std::endl;
+  
+  i = 0;
+  Patterns patterns2;
+  for (auto x : notDoubles1) {
+    for (auto c : x.hash) {
+      if (c == '_') {
+        ++i;
+      }
+    }
+    patterns2.hashTable[x.hash] = x;
+  }
+  std::cout << i << std::endl;
+
+  patterns2.save("generatedPatterns.pat");
+  std::cout << patterns2.hashTable.size() << std::endl;
+
+}
