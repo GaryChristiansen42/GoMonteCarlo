265c434502e1687a7f8925437b29fc4804c46184
Gary Christiansen <digimon2.0@gmail.com>
2015-02-07 23:47:29 -0600

    Changed the playGnuGo script.

--- KGSClient/playGnuGo.sh
+++ KGSClient/playGnuGo.sh
@@ -1,1 +1,10 @@
-python ../twogtp.py --black 'gnugo --mode gtp --boardsize 9 --chinese-rules --level 1' --white './GTPClient | tee testOutput' --size 9 --verbose 2 --games 10
+#python ../twogtp.py --black './GTPClient | tee testOutput' --white 'gnugo --mode gtp --boardsize 9 --chinese-rules --level 1' --size 9 --games 10
+#python ../twogtp.py --black 'gnugo --mode gtp --boardsize 9 --chinese-rules --level 1' --white './GTPClient | tee testOutput' --size 9 --games 10
+
+#echo "GnuGoVsGnuGoLevel10-9x9"
+#python ../twogtp.py --black 'gnugo --mode gtp --boardsize 9 --chinese-rules --level 1' --white 'gnugo --mode gtp --boardsize 9 --chinese-rules --level 10' --size 9 --games 100 
+#echo "GnuGoVsGnuGoLevel10-19x19"
+#python ../twogtp.py --black 'gnugo --mode gtp --boardsize 19 --chinese-rules --level 1' --white 'gnugo --mode gtp --boardsize 19 --chinese-rules --level 10' --size 19 --games 100 
+echo "GnuGoVsGnuGoLevel10-9x9"
+python ../twogtp.py --black './GTPClient | tee testOutput' --white 'gnugo --mode gtp --boardsize 9 --chinese-rules --level 10' --size 9 --games 100 
+echo "End"
